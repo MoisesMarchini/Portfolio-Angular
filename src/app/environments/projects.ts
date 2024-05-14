24433ee02d713d9bd34f5e043f4f8a39a16c63d0
Moises Marchini <moisesmarchinipereira@gmail.com>
2024-05-14 12:47:21 -0300

    Att: project-cards;

---
+++ src/app/environments/projects.ts
@@ -0,0 +1,229 @@
+export const projects = [
+  {
+    title: 'Clean Home',
+    tags: [
+      {
+        title: 'Angular',
+        fontAwesomeIcon: 'fa-brands fa-angular',
+      },
+      {
+        title: 'Sass',
+        fontAwesomeIcon: 'fa-brands fa-sass',
+      },
+    ],
+    links: {
+      demo: 'https://green-thumb-git-variants-clean-home-moisesmarchinis-projects.vercel.app/',
+      github:
+        'https://github.com/MoisesMarchini/Template-GreenThumb/tree/variants%2Fclean-home',
+    },
+    background: '/assets/images/projects/clean-home-cover.png',
+    description: '',
+  },
+  {
+    title: 'PetPal',
+    tags: [
+      {
+        title: 'Angular',
+        fontAwesomeIcon: 'fa-brands fa-angular',
+      },
+      {
+        title: 'Sass',
+        fontAwesomeIcon: 'fa-brands fa-sass',
+      },
+    ],
+    links: {
+      demo: 'https://green-thumb-git-variants-petpal-moisesmarchinis-projects.vercel.app/',
+      github:
+        'https://github.com/MoisesMarchini/Template-GreenThumb/tree/variants%2Fpetpal',
+    },
+    background: '/assets/images/projects/petpal-cover.png',
+    description: '',
+  },
+  {
+    title: 'Green Thumb',
+    tags: [
+      {
+        title: 'Angular',
+        fontAwesomeIcon: 'fa-brands fa-angular',
+      },
+      {
+        title: 'Sass',
+        fontAwesomeIcon: 'fa-brands fa-sass',
+      },
+    ],
+    links: {
+      demo: 'https://green-thumb-three.vercel.app/',
+      github:
+        'https://github.com/MoisesMarchini/Template-GreenThumb/tree/master',
+    },
+    background: '/assets/images/projects/green-thumb-cover.png',
+    description: '',
+  },
+  {
+    title: 'Fresh Cleaning Team',
+    tags: [
+      {
+        title: 'Angular',
+        fontAwesomeIcon: 'fa-brands fa-angular',
+      },
+      {
+        title: 'Sass',
+        fontAwesomeIcon: 'fa-brands fa-sass',
+      },
+    ],
+    links: {
+      demo: 'https://freshcleaningteam.com/',
+    },
+    background: '/assets/images/projects/freshcleaningteam-cover.png',
+    description: '',
+  },
+  {
+    title: 'Fernandex Portfolio',
+    tags: [
+      {
+        title: 'Angular',
+        fontAwesomeIcon: 'fa-brands fa-angular',
+      },
+      {
+        title: 'Sass',
+        fontAwesomeIcon: 'fa-brands fa-sass',
+      },
+    ],
+    links: {
+      demo: 'https://fernandex.tech/',
+    },
+    background: '/assets/images/projects/fernandex-cover.png',
+    description: '',
+  },
+  {
+    title: 'Medical Victory',
+    tags: [
+      {
+        title: 'Figma',
+        fontAwesomeIcon: 'fa-brands fa-figma',
+      },
+      {
+        title: 'Angular',
+        fontAwesomeIcon: 'fa-brands fa-angular',
+      },
+      {
+        title: 'Sass',
+        fontAwesomeIcon: 'fa-brands fa-sass',
+      },
+    ],
+    links: {
+      demo: 'https://mmp-medical-victory.vercel.app/',
+      github: 'https://github.com/MoisesMarchini/LandingPage-MedicalVictory',
+    },
+    background: '/assets/images/projects/MedicalVictory-cover.png',
+    description: '',
+  },
+  {
+    title: 'EJAM',
+    tags: [
+      {
+        title: 'Figma',
+        fontAwesomeIcon: 'fa-brands fa-figma',
+      },
+      {
+        title: 'Angular',
+        fontAwesomeIcon: 'fa-brands fa-angular',
+      },
+      {
+        title: 'Sass',
+        fontAwesomeIcon: 'fa-brands fa-sass',
+      },
+    ],
+    links: {
+      demo: 'https://ejam-angular.vercel.app/',
+      github: 'https://github.com/MoisesMarchini/Ejam-Angular',
+    },
+    background: '/assets/images/projects/ejam-cover.png',
+    description: '',
+  },
+  {
+    title: 'TaskMaster',
+    tags: [
+      {
+        title: 'Angular',
+        fontAwesomeIcon: 'fa-brands fa-angular',
+      },
+      {
+        title: 'Sass',
+        fontAwesomeIcon: 'fa-brands fa-sass',
+      },
+    ],
+    links: {
+      demo: 'https://mmp-taskmaster.vercel.app/',
+      github: 'https://github.com/MoisesMarchini/TaskMaster',
+    },
+    background: '/assets/images/projects/taskmaster-cover.png',
+    description: '',
+  },
+  {
+    title: 'FurryHaven',
+    tags: [
+      {
+        title: 'Angular',
+        fontAwesomeIcon: 'fa-brands fa-angular',
+      },
+      {
+        title: 'Sass',
+        fontAwesomeIcon: 'fa-brands fa-sass',
+      },
+    ],
+    links: {
+      demo: 'https://mmp-furryhaven.vercel.app/',
+      github: 'https://github.com/MoisesMarchini/LandingPage-FurryHaven',
+    },
+    background: '/assets/images/projects/furryhaven-cover.png',
+    description: '',
+  },
+  {
+    title: 'Pokedex',
+    tags: [
+      {
+        title: 'Angular',
+        fontAwesomeIcon: 'fa-brands fa-angular',
+      },
+      {
+        title: 'Sass',
+        fontAwesomeIcon: 'fa-brands fa-sass',
+      },
+    ],
+    links: {
+      demo: 'https://mmp-pokedex.vercel.app/',
+      github: 'https://github.com/MoisesMarchini/Pokedex',
+    },
+    background: '/assets/images/projects/pokedex-cover.png',
+    description: '',
+  },
+  {
+    title: 'Tiny Despair',
+    tags: [
+      {
+        title: 'Unity',
+        fontAwesomeIcon: 'fa-brands fa-unity',
+      },
+    ],
+    links: {
+      demo: 'https://moisesmp.itch.io/tiny-despair',
+    },
+    background: '/assets/images/projects/tinydespair-cover.png',
+    description: '',
+  },
+  {
+    title: 'Super NiceBoy',
+    tags: [
+      {
+        title: 'Unity',
+        fontAwesomeIcon: 'fa-brands fa-unity',
+      },
+    ],
+    links: {
+      demo: 'https://moisesmp.itch.io/superniceboy',
+    },
+    background: '/assets/images/projects/superniceboy-cover.png',
+    description: '',
+  },
+];
