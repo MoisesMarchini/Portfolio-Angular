74668c74bd2e13a2887b7db7d3ac3e0298555826
Moises Marchini <moisesmarchinipereira@gmail.com>
2024-06-20 03:38:17 -0300

    feat: twMerge package;

--- src/app/shared/components/bento-grid/block/block.component.ts
+++ src/app/shared/components/bento-grid/block/block.component.ts
@@ -1,4 +1,5 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component, ElementRef, HostBinding } from '@angular/core';
+import { twMerge } from 'tailwind-merge';
 
 @Component({
   selector: 'block',
@@ -6,7 +7,12 @@ import { Component, HostBinding } from '@angular/core';
   template: `<ng-content></ng-content>`,
 })
 export class BlockComponent {
+  constructor(public elementRef: ElementRef) {}
+
   @HostBinding('class') get classNames() {
-    return 'block col-span-4 bg-zinc-800 border-zinc-700 border rounded p-4 text-white';
+    return twMerge(
+      'block col-span-4 bg-zinc-800 border-zinc-700 border rounded p-4 text-white',
+      this.elementRef.nativeElement.className
+    );
   }
 }
--- src/app/shared/components/bento-grid/socials/socials.component.ts
+++ src/app/shared/components/bento-grid/socials/socials.component.ts
@@ -1,12 +1,17 @@
 import { Component, HostBinding } from '@angular/core';
+import { BlockComponent } from '../block/block.component';
+import { twMerge } from 'tailwind-merge';
 
 @Component({
   selector: 'socials',
   templateUrl: './socials.component.html',
   styleUrls: ['./socials.component.scss'],
 })
-export class SocialsComponent {
-  @HostBinding('class') get classNames() {
-    return 'col-span-12 md:col-span-6';
+export class SocialsComponent extends BlockComponent {
+  @HostBinding('class') override get classNames() {
+    return twMerge(
+      'col-span-12 md:col-span-6',
+      this.elementRef.nativeElement.className
+    );
   }
 }
